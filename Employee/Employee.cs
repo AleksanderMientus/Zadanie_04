27b542f443853f1e39c31ce7b9f5f4f5587690d6
Aleksander Mientus <aleksander.mientus@synevo.pl>
2021-07-05 13:55:51 +0200

    Add project files.

---
+++ Employee/Employee.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Employee
+    {
+        private string nazwisko;
+        private string imie;
+        private double stawkaGodzinowa;
+        private double pensja;
+        private Dictionary<string, double> czasPracy = new Dictionary<string, double>(2);
+
+        /// <summary>
+        /// Definicja pracownika
+        /// </summary>
+        /// <param name="imie">string imię pracownika</param>
+        /// <param name="nazwisko">string nazwisko pracownika</param>
+        /// <param name="stawkaGodzinowa">stawka za godzinę pracy pracownika</param>
+        public Employee(string imie, string nazwisko, double stawkaGodzinowa)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.stawkaGodzinowa = stawkaGodzinowa;
+            pensja = 0.00;
+            czasPracy.Add("Normalne", 0.00);
+            czasPracy.Add("Nadgodziny", 0.00);
+            EmployeeInfo();
+        }
+
+        /// <summary>
+        /// Rejestruje czas pracy pracownika i zlicza jego pensję.
+        /// </summary>
+        /// <param name="przepracowaneGodziny">double przepracowane godziny w danym dniu</param>
+        public void RegisterTime(double przepracowaneGodziny)
+        {
+            if (przepracowaneGodziny <= 8) 
+            {
+                pensja += stawkaGodzinowa * przepracowaneGodziny;
+                czasPracy["Normalne"] += przepracowaneGodziny;
+            }
+            else
+            {
+                pensja += (stawkaGodzinowa * 8) + (stawkaGodzinowa * 2 * (przepracowaneGodziny - 8));
+                czasPracy["Normalne"] += 8;
+                czasPracy["Nadgodziny"] += przepracowaneGodziny - 8;
+            }
+        }
+
+        public void EmployeeInfo()
+        {
+            Console.WriteLine("Pracownik:");
+            Console.WriteLine("{0,-20}{1,7:0.00}", "Nazwisko i imię: ", nazwisko + " " + imie);
+            Console.WriteLine("{0,-20}{1,7:0.00}", "Stawka godzinowa: ", stawkaGodzinowa);
+        }
+        /// <summary>
+        /// Wypłata pensji.
+        /// </summary>
+        public void PaySalary()
+        {
+            Console.WriteLine("\n------------------");
+            Console.WriteLine("  WYPŁATA PENSJI");
+            Console.WriteLine("------------------");
+            EmployeeInfo();
+            Console.WriteLine("{0,-20}{1,7:0.00}", "Do wypłaty: ", pensja);
+            Console.WriteLine("Czas pracy:");
+            Console.WriteLine("{0,-20}{1,7:0.00}", "Normalne godziny: ", czasPracy["Normalne"]);
+            Console.WriteLine("{0,-20}{1,7:0.00}", "Nadgodziny: ", czasPracy["Nadgodziny"]);
+            // zerowanie pensji po wypłacie
+            pensja = 0;
+            czasPracy["Normalne"] = 0;
+            czasPracy["Nadgodziny"] = 0;
+        }
+    }
+}
---
+++ Employee/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Program
+    {
+        /// <summary>
+        /// Zaimplementuj klasę `Employee` umożliwiającą rejestrowanie czasu pracy oraz
+        /// wypłacanie pensji na podstawie zadanej stawki godzinowej. Jeżeli pracownik
+        /// będzie pracował więcej niż 8 godzin (podczas pojedynczej rejestracji czasu) to
+        /// kolejne godziny policz jako nadgodziny (z podwójną stawką godzinową).
+        /// 
+        /// Konstruktor klasy niech przyjmuje imię, nazwisko i stawkę godzinową.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("Jan", "Nowak", 100.0);
+            emp1.RegisterTime(5);   //za to naliczy mu się 5*100
+            emp1.RegisterTime(10);  //za to naliczy mu się 8*100 + 2*200
+            emp1.RegisterTime(7.5); //za to naliczy mu się 7.5*100
+            emp1.PaySalary(); //wyplacanie = stan należności po stronie pracodawcy = 0
+            emp1.RegisterTime(11);  //za to naliczy mu się 8*100 + 3*200
+            emp1.PaySalary();
+
+            Console.ReadKey();
+        }
+    }
+}
